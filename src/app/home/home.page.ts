9b5d4278ad48a519503c5ed34a414cddd880418b
HejdaJakub <kuba.hejda97@gmail.com>
2024-02-12 20:51:44 +0100

    add the rest of changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  photo: SafeResourceUrl | undefined;
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer, private httpClient: HttpClient) {}
 
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    console.log(image)
+
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(String(image.dataUrl));
+
+    console.log(this.photo)
+  }
 }
